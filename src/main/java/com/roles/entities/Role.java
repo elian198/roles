ac2019d104f92867b9a66657fcd065c5b09a0381
Elian <elianpareja87@hotmail.com>
2022-11-04 09:21:19 -0300

    create entities Role

--- src/main/java/com/roles/entities/Role.java
+++ src/main/java/com/roles/entities/Role.java
@@ -1,4 +1,44 @@
 package com.roles.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ROLES")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "NAME",nullable = false)
+    private String name;
+
+    @Column(name = "DESCRIPTION", nullable = false)
+    private String description;
+
+    public Role() { }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
