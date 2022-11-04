3fc5da0a898ad826c472d2f9b35cc0b5852c9096
Elian <elianpareja87@hotmail.com>
2022-11-04 18:38:44 -0300

    create Repository Role and User

---
+++ src/main/java/com/roles/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.roles.repository;
+
+import com.roles.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
