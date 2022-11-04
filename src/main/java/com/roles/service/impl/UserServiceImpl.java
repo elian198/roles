4527b65cd50c5f145f02e33ad5580bb6c7c6e21a
Elian <elianpareja87@hotmail.com>
2022-11-04 20:19:53 -0300

    Method findById() created

--- src/main/java/com/roles/service/impl/UserServiceImpl.java
+++ src/main/java/com/roles/service/impl/UserServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -54,16 +55,16 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> findAll() {
-        return null;
+        return  repository.findAll();
     }
 
     @Override
     public User findOne(String username) {
-        return null;
+        return repository.findByUsername(username);
     }
 
     @Override
-    public User findById(Long id) {
-        return null;
+    public Optional<User> findById(Long id) {
+        return repository.findById(id);
     }
 }
