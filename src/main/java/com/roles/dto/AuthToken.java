4667c9b5b066d5fe480b768dd28205228ba5e11c
Elian <elianpareja87@hotmail.com>
2022-11-07 17:12:15 -0300

    DTO token

---
+++ src/main/java/com/roles/dto/AuthToken.java
@@ -0,0 +1,18 @@
+package com.roles.dto;
+
+public class AuthToken {
+
+    private String token;
+
+    public AuthToken(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
