0cf4ab65b286b1c9f4d7210ae8f6340de9efbcb3
Elian <elianpareja87@hotmail.com>
2022-11-09 11:02:58 -0300

    Modify UserDTO.class

--- src/main/java/com/roles/dto/UserDTO.java
+++ src/main/java/com/roles/dto/UserDTO.java
@@ -4,6 +4,8 @@ import com.roles.entities.User;
 
 public class UserDTO {
 
+    private Long id;
+
     private String userName;
 
     private String email;
@@ -32,12 +34,23 @@ public class UserDTO {
         return password;
     }
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
     public void setPassword(String password) {
         this.password = password;
     }
 
     public User transformToUser(){
     User user = new User();
+    user.setId(id);
     user.setUserName(userName);
     user.setEmail(email);
     user.setPassowrd(password);
